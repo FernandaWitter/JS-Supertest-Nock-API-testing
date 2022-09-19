4069f656414e624a73f91ea7f3e76cc761b1fa18
Fernanda Witter <fernanda.witter@muralis.com.br>
2022-09-19 15:14:56 -0300

    feat: adds response tests

---
+++ test/response.spec.js
@@ -0,0 +1,29 @@
+const app = require('../src/app.js')
+const request = require('supertest')
+const expect = require('chai').expect
+
+describe('Response tests', () => {
+    it('JSON response', () => {
+        request(app)
+            .get('/course')
+            .end((err, res) => {
+                expect(res.body.id).to.be.equal('1')
+            })
+    })
+
+    it('Text response', () => {
+        request(app)
+            .get('/course')
+            .end((err, res) => {
+                expect(res.text).to.contain('1')
+            })
+    })
+
+    it('Status response', () => {
+        request(app)
+            .get('/course')
+            .end((err, res) => {
+                expect(res.ok).to.be.true
+            })
+    })
+})
